7a3b5b3ff0ce72f935bfe6fc873efa58f4e61140
torandi <torandi@71c61736-3055-43ec-afd9-7e59d617a297>
2010-07-05 23:03:34 +0000

    Working with grafics
    git-svn-id: https://svn.torandi.com/rocket@10 71c61736-3055-43ec-afd9-7e59d617a297

--- c_client/gfx.c
+++ c_client/gfx.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 #include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_image.h>
 #include <SDL/SDL_gfxPrimitives.h>
 
 #include "gfx.h"
@@ -8,7 +10,7 @@
 #define SCREEN_WIDTH  640 // width of the screen, in pixels
 #define SCREEN_HEIGHT 480 // height of the screen, in pixels
 
-#define SHIP_SIZE 14 
+#define SHIP_SIZE 32
 #define HALF_SHIP_SIZE SHIP_SIZE/2
 
 #define PI 3.14159265
@@ -16,24 +18,29 @@
 SDL_Surface *screen; 
 SDL_Event    event;
 SDL_Rect	screen_area;
+TTF_Font * font;
+SDL_Surface * ship;
+SDL_Color font_color={0xFF,0xFF,0xFF};
 
 void slock();
 void sulock();
 int radians_to_degrees(double rad);
 double degrees_to_radians(int d);
+TTF_Font* loadfont(char* file, int ptsize);
 
-/*
+
 void main() {
 	init_sdl();
 	while(1) {
-	draw_ship(100,100,45);
+	draw_ship("Torandi",100,100,45,NULL,0);
+	gfx_update();
 		if(hndl_sdl_events()!=0) {
 			break;
 			exit(0);
 		}
 		sleep(100);
 	}
-}*/
+}
 
 int init_sdl() {
 	if (SDL_Init( SDL_INIT_VIDEO ) != 0) {
@@ -43,15 +50,28 @@ int init_sdl() {
 
 	} // if (could not init video)
 
+	if (TTF_Init() == -1) {
+		fprintf(stderr,"Unable to initialize SDL_ttf: %s \n", TTF_GetError());
+		return 2;
+	}
+
+	font=loadfont("Acknowledge_TT_BRK.ttf",sizeof("Acknowledge_TT_BRK.ttf"));
+
 	if ((screen =
 				SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, 8, SDL_SWSURFACE ))
 			== NULL) {
 
 		fprintf( stderr, "Could not set SDL video mode: %s\n", SDL_GetError() );
-		return 2;
+		return 3;
 	
 	SDL_WM_SetCaption( "Rocket - Robot Sockets", "Rocket - Robot Sockets" );
 	SDL_ShowCursor( SDL_DISABLE );
+
+	ship=IMG_Load("ship.png");
+	if(!ship) {
+		    printf("IMG_Load: %s\n", IMG_GetError());
+			 return 4;
+	}
 
 	} // if (could not set mode)
 	screen_area.x=0;
@@ -60,6 +80,16 @@ int init_sdl() {
 	screen_area.h=SCREEN_HEIGHT;
 
 	return 0;
+}
+
+TTF_Font* loadfont(char* file, int ptsize) {
+	  TTF_Font* tmpfont;
+	    tmpfont = TTF_OpenFont(file, ptsize);
+		   if (tmpfont == NULL){
+				    printf("Unable to load font: %s %s \n", file, TTF_GetError());
+					     // Handle the error here.
+					   }
+			  return tmpfont;
 }
 
 /*
@@ -89,7 +119,16 @@ void draw_ship(char nick[32],int x, int y, int a,gfx_attr_t *attr,int num_attr) 
 	
 	slock();
 
-	aatrigonColor(screen, x+SHIP_SIZE*cos(angle),y+SHIP_SIZE*sin(angle),x+SHIP_SIZE*cos(angle+2*PI/3),y+SHIP_SIZE*sin(angle+2*PI/3),x+SHIP_SIZE*cos(angle+4*PI/3),y+SHIP_SIZE*sin(angle+4*PI/3),color);
+	//SDL_Surface *ship_surface;
+	SDL_Rect ship_pos={x,y,SHIP_SIZE,SHIP_SIZE};
+	SDL_BlitSurface(ship,NULL,screen,NULL);
+
+	//aatrigonColor(screen, x+SHIP_SIZE*cos(angle),y+SHIP_SIZE*sin(angle),x+SHIP_SIZE*cos(angle+2*PI/3),y+SHIP_SIZE*sin(angle+2*PI/3),x+SHIP_SIZE*cos(angle+4*PI/3),y+SHIP_SIZE*sin(angle+4*PI/3),color);
+
+	SDL_Surface *text_surface=TTF_RenderText_Blended(font,nick,font_color);
+	SDL_Rect text_rect={x-40,y-HALF_SHIP_SIZE-30,16,6};
+	SDL_BlitSurface(text_surface,NULL,screen,&text_rect);
+	SDL_FreeSurface(text_surface);
 
 	sulock();
 	//Update area
