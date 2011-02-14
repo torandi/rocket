abfc1d34089a711ff0de9ce07001b28af0e74291
nsg <nsg@71c61736-3055-43ec-afd9-7e59d617a297>
2011-02-14 22:24:45 +0000

    randombot
    git-svn-id: https://svn.torandi.com/rocket@70 71c61736-3055-43ec-afd9-7e59d617a297

---
+++ robots/ruby-examples/robot_random.rb
@@ -0,0 +1,28 @@
+# Ruby example bot
+# This is simple bot, the bot move around.
+
+require 'socket'
+
+# Useful for debugning
+Thread.abort_on_exception = true
+
+# You need to connect to the display
+hostname = 'localhost'
+port = 4710
+
+# Open connection
+s = TCPSocket.open(hostname, port)
+
+s.puts "name randombot"
+
+# Enter main loop
+while true
+  a = rand(360)
+  puts a
+  s.puts "angle #{a}"
+  s.puts "boost" if rand(1) == 0
+  s.puts "shoot" if rand(2) == 0
+  sleep 1
+end
+
+s.close
