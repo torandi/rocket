2fbd27dc0c70487c803a6b585d9963686cf2fcb0
nsg <nsg@71c61736-3055-43ec-afd9-7e59d617a297>
2011-06-02 23:32:15 +0000

    A working package impl (enc. disabled).
    git-svn-id: https://svn.torandi.com/rocket@105 71c61736-3055-43ec-afd9-7e59d617a297

--- ruby_server/protocol.rb
+++ ruby_server/protocol.rb
@@ -4,20 +4,31 @@ class Protocol
 
   def self.read client
   	begin
-      package = client.read(32)
-      frame = package.bytes.map{ |a| (a^5).chr }.to_s
-      len = frame[0] # note, vid ruby 1.9 lägg på .ord
-      hash = frame[1..4]
-      payload = frame[(32-len)..-1]
-      puts "read: #{package} -> [#{frame}] #{payload}"
-      if payload.nil?
-        puts "error: nil payload"
-        return nil
-      end
-      chash = Digest::SHA1.digest(payload)[0..3]
-      if chash != hash
-        puts "error: hash missmatch package:#{hash} calc:#{chash}"
-        return nil
+  	  run = true
+  	  payload = ""
+  	  while run
+        package = client.read(32)
+        if package.nil?
+          puts "recived nil"
+          return nil
+        end
+        frame = package #.bytes.map{ |a| (a^5).chr }.to_s
+        len = frame[0] # note, vid ruby 1.9 lägg på .ord
+        hash = frame[1..4]
+        payload = payload + frame[(32-(len == 0 ? 27 : len))..-1]
+        #puts "read: #{package} -> [#{frame}] '#{payload}'"
+        if payload.nil?
+          puts "error: nil payload"
+          return nil
+        end
+        chash = Digest::SHA1.hexdigest(payload)[0..3]
+        #package.bytes.each { |x| puts x }
+        if chash != hash
+          puts "error: hash missmatch package:#{hash} calc:#{chash}"
+          return nil
+        end
+        
+        run = false if len > 0
       end
 	 	rescue Errno::ECONNRESET
 	 		nil
@@ -30,14 +41,15 @@ class Protocol
       Protocol.write client, str[0..26], true
       Protocol.write client, str[27..-1]
     else
-      len = overflow ? 0 : str.length.chr
-      hash = Digest::SHA1.digest(str)[0..3]
-      padding = (27-len).times.map{ rand(9) }.join
+      len = overflow ? 0 : str.length
+      hash = Digest::SHA1.hexdigest(str)[0..3]
+      padding = (27-(len == 0 ? 27 : len)).times.map{ rand(9) }.join
       payload = str
-      frame = "#{len}#{hash}#{padding}#{payload}"
-      package = frame.bytes.map{ |a| (a^5).chr }.to_s
-      client.puts package
-      puts "write: #{str} [#{frame}] -> #{package}"
+      frame = "#{len.chr}#{hash}#{padding}#{payload}"
+      package = frame #.bytes.map{ |a| (a^5).chr }.to_s
+      client.write package
+      puts "ws: #{str}"
+      puts "wp:#{package}"
     end
   end
 
